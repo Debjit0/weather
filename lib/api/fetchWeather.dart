86304cd67f4dfbf09425f1a0b478bc457ed4cf2a
Debjit Sarkar <68617086+Debjit0@users.noreply.github.com>
2023-07-11 11:41:13 +0530

    apistuff

--- lib/api/fetchWeather.dart
+++ lib/api/fetchWeather.dart
@@ -1,13 +1,24 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:weather_app/api/apikey.dart';
+
 import '../model/weather_data.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class FetchWeatherAPI{
   WeatherData? weatherData;
 
   //processing data from response to json
   Future<WeatherData> processData(lat,lon)async{
-    var response = await http.get
+    var response = await http.get(Uri.parse(apiURL(lat, lon)));
+    var jsonString = jsonDecode(response.body);
+    
+  }
+
+  String apiURL(var lat, var lon){
+    String url;
+    url="https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$lon&appid=$apikey";
+    return url;
   }
 }
